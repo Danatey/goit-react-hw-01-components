63a5fe2df582e2a808b0739068e7f61891e8f11f
Данатей <danatey40793@gmail.com>
2021-07-11 15:45:53 +0300

    +TransactionHistory

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './components/Profile';
 import Statistics from './components/Statistics';
 import FriendList from './components/FriendList';
+import TransactionHistory from './components/TransactionHistory';
 
 import user from './social-profile/user.json';
 import statisticalData from './statistics/statistical-data.json';
 import friends from './friend-list/friends.json';
+import transactions from './transaction-history/transactions.json';
 
 function App() {
     return <div>
@@ -15,9 +17,10 @@ function App() {
             avatar={user.avatar}
             stats={user.stats}
         />
-        <Statistics title="Upload stats" stats={statisticalData} />;
-        <Statistics stats={statisticalData} />;
-        <FriendList friends={friends} />,
+        <Statistics title="Upload stats" stats={statisticalData} />
+        <Statistics stats={statisticalData} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
     </div>
 }
 
